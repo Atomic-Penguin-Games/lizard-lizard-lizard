75c97dfac13ccee42578adbac7ef034d404c949f
Derek Briggs <contact@derekbriggs.xyz>
2025-08-02 00:07:04 -0400

    Load and draw character sprite

--- src/main.c
+++ src/main.c
@@ -1,18 +1,32 @@
 #include "raylib.h"
 
+typedef struct Player
+{
+  Vector2 position;
+} Player;
+
+
 int main(void)
 {
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "{#gameName}");
+    InitWindow(screenWidth, screenHeight, "Lizard Meme");
     SetTargetFPS(60);
+
+    Texture sprite = LoadTexture("res/lizardSprite.png");
+
+    Player player = {
+      .position = { 50, 50}
+    };
 
     while (!WindowShouldClose())
     {
       BeginDrawing();
       ClearBackground(RED);
-      DrawText("Hola Mundo\n{#gameName}", screenWidth/2, screenHeight/2, 40, DARKGRAY);
+      DrawText("Hola Mundo\nLizard Meme", screenWidth/2, screenHeight/2, 40, DARKGRAY);
+      DrawTextureEx(sprite, player.position, 0.0f, 0.25f, RAYWHITE);
+      
       EndDrawing();
     }
 
